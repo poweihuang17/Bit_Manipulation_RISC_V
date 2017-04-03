8745b167b79fa319b86fd01ae75c8001ab999fb1
Po-wei Huang <poweihuang17@gmail.com>
2017-04-03 23:11:39 +0800

    Finish the CLZ.v.

--- CLZ.v
+++ CLZ.v
@@ -19,17 +19,17 @@ always@(*)
         2'b00:
             begin
             //Still need to define the result of all 0.
-            rd[4]=0;
+            rd[4]=1'b0;
             result_stage1=rs1[15:0];
             end
         2'b11,2'b10:
             begin
-            rd[4]=0;
+            rd[4]=1'b0;
             result_stage1=rs1[31:16];
             end
         2'b01:
             begin
-            rd[4]=1;
+            rd[4]=1'b1;
             result_stage1=rs1[15:0];
             end
     end
@@ -46,17 +46,17 @@ always@(*)
         2'b00:
             begin
             //Still need to define the result of all 0.
-            rd[3]=0;
+            rd[3]=1'b0;
             result_stage2=result_stage[7:0];
             end
         2'b11,2'b10:
             begin
-            rd[3]=0;
+            rd[3]=1'b0;
             result_stage2=result_stage1[15:8];
             end
         2'b01:
             begin
-            rd[3]=1;
+            rd[3]=1'b1;
             result_stage2=result_stage1[7:0];
             end
     end
@@ -74,26 +74,26 @@ always@(*)
         2'b00:
             begin
             //Still need to define the result of all 0.
-            rd[2]=0;
+            rd[2]=1'b0;
             result_stage3=result_stage2[3:0];
             end
         2'b11,2'b10:
             begin
-            rd[2]=0;
+            rd[2]=1'b0;
             result_stage3=result_stage2[7:4];
             end
         2'b01:
             begin
-            rd[2]=1;
+            rd[2]=1'b1;
             result_stage3=result_stage2[3:0];
             end
     end
 
 //Fourth stage
-wire or_upper_4,or_down_4;
-assign or_upper_4=|result_stage3[7:4];
-assign or_down_4=|result_stage3[3:0];
-assign condition_4={or_upper_4,or_down_4};
+wire or_upper_2,or_down_2;
+assign or_upper_2=|result_stage3[3:2];
+assign or_down_2=|result_stage3[1:0];
+assign condition_2={or_upper_2,or_down_2};
 wire [3:0] result_stage4;
 
 always@(*)
@@ -102,26 +102,25 @@ always@(*)
         2'b00:
             begin
             //Still need to define the result of all 0.
-            rd[1]=0;
-            result_stage4=result_stage3[3:0];
+            rd[1]=1'b0;
+            result_stage4=result_stage3[1:0];
             end
         2'b11,2'b10:
             begin
-            rd[1]=0;
-            result_stage4=result_stage3[7:4];
+            rd[1]=1'b0;
+            result_stage4=result_stage3[3:2];
             end
         2'b01:
             begin
-            rd[1]=0;
-            result_stage4=result_stage3[3:0];
+            rd[1]=1'b0;
+            result_stage4=result_stage3[1:0];
             end
     end
 
 
 
 //Fifth stage
-assign condition_1={or_upper_16,or_down_16};
-wire [15:0] result_stage1;
+assign condition_1=result_stage4;
 
 always@(*)
     begin
@@ -129,35 +128,15 @@ always@(*)
         2'b00:
             begin
             //Still need to define the result of all 0.
-            rd[4]=0;
-            result_stage1=rs1[15:0];
+            rd[0]=1'b0;
             end
         2'b11,2'b10:
             begin
-            rd[4]=0;
-            result_stage1=rs1[31:16];
+            rd[0]=1'b0;
             end
         2'b01:
             begin
-            rd[4]=1;
-            result_stage1=rs1[15:0];
+            rd[0]=1'b1;
             end
     end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 endmodule
